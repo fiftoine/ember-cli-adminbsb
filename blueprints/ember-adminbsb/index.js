c7d53c455dac84fa2e49b7caefc95132efa99c09
Antoine <antoine.dubois@gmail.com>
2018-03-16 09:18:33 +0100

    Added ember-changeset-validation-i18n

--- blueprints/ember-adminbsb/index.js
+++ blueprints/ember-adminbsb/index.js
@@ -19,7 +19,8 @@ module.exports = {
           {name: 'ember-toggle-helper', target: '0.1.1'},
           {name: 'ember-cli-range-slider', target: '0.2.7'},
           {name: 'ember-cli-bootstrap-colorpicker', target: '2.5.1'},
-          {name: 'ember-changeset-validations', target:'1.2.11'}
+          {name: 'ember-changeset-validations', target:'1.2.11'},
+          {name: 'ember-i18n-changeset-validations', target:'1.1.0'}
 
         ]
       })
